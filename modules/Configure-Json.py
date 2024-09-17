cf42da9db04ca9b1a20416793678166eeee89aa8
Shawn Tan <banchan@gmail.com>
2024-09-17 00:32:43 -0700

    Refactor and add checks

--- modules/Configure-Json.py
+++ modules/Configure-Json.py
@@ -1,12 +1,9 @@
-# code based on comparing bonsai.gui docfx.json file to a fresh docfx.json (updated 4/27/24)
-# might be easier to just copy the docfx.json file but it might change when docfx is updated.
 import json
 import os
 
 # define folder constants
 current_dir = os.getcwd()
-root_dir = os.path.dirname(os.path.dirname(os.path.dirname(current_dir)))
-docs_dir = os.path.join(root_dir,"docs")
+docs_dir = os.path.dirname(os.path.dirname(current_dir))
 
 # load json file
 try:
@@ -17,66 +14,32 @@ except FileNotFoundError:
     print(exit_string)
     os.exit(1)
 
-# prompt for github website
-github_link = input("Please enter the github website: ")
-
 # adds filter .yml to metadata build
-# the structure in the attributes is a bit weird, its a dictionary in a list, so the index [0] is just calling up that dictionary
 data["metadata"][0]["filter"] = "filter.yml"
 
-# adds files and folders to be excluded from site building
-if "filter.yml" in data["build"]["content"][0]["exclude"]:
-    pass
-else:
-    data["build"]["content"][0]["exclude"].extend(["filter.yml"])
+# adds filter.yml to be excluded from site building
+if "filter.yml" not in data["build"]["content"][0]["exclude"]:
+    data["build"]["content"][0]["exclude"].append("filter.yml")
                                                   
-if "bonsai/**" in data["build"]["content"][0]["exclude"]:
-    pass
-else:
-    data["build"]["content"][0]["exclude"].extend(["bonsai/**"])
-
 # adds site resources
-if "workflows/**" in data["build"]["resource"][0]["files"]:
-    pass
-else: 
-    data["build"]["resource"][0]["files"].extend(["workflows/**"])
-    
-if "logo.svg" in data["build"]["resource"][0]["files"]:
-    pass
-else: 
-    data["build"]["resource"][0]["files"].extend(["logo.svg"])
-
-if "favicon.ico" in data["build"]["resource"][0]["files"]:
-    pass
-else: 
-    data["build"]["resource"][0]["files"].extend(["favicon.ico"])
+data["build"]["resource"][0]["files"].extend(
+    [file for file in ["workflows/**", "logo.svg", "favicon.ico"] if file not in data["build"]["resource"][0]["files"]]
+)
 
 # adds overwrite section
 data["build"]["overwrite"] = [{"files":["apidoc/**.md"], 
                                  "exclude": ["obj/**", "_site/**"]}]
 
 # adds bonsai templates 
-if "bonsai/template" in data["build"]["template"]:
-    pass
-else:
-    data["build"]["template"].extend(["bonsai/template"])
-    
-if "template" in data["build"]["template"]:
-    pass
-else:
-    data["build"]["template"].extend(["template"])
+data["build"]["template"].extend(
+    [item for item in ["bonsai/template", "template"] if item not in data["build"]["template"]]
+)
 
 # adds site footer
 data["build"]["globalMetadata"]["_appFooter"] = "&copy; 2024 Bonsai Foundation CIC and Contributors. Made with <a href=\"https://dotnet.github.io/docfx\">docfx</a>"
 
-# adds git contribute section
-data["build"]["globalMetadata"]["_gitContribute"] = {"repo": github_link, 
-                                                     "branch": "main", 
-                                                     "apiSpecFolder": "apidoc"}
-
 # adds markdig extensions
-data["build"]["markdownEngineProperties"] = {"markdigExtensions": ["attributes",
-                                                                   "customcontainers"]}
+data["build"]["markdownEngineProperties"] = {"markdigExtensions": ["attributes", "customcontainers"]}
 
 # adds xref 
 data["build"]["xref"] = ["https://bonsai-rx.org/docs/xrefmap.yml"]
